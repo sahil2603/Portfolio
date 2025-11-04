842a8d9106305dc97560414ea6c83c076ebbe5c7
Sahil Kumar <sahil.sah2603@gmail.com>
2025-11-04 16:56:29 +0530

    Added typing animation under name

--- script.js
+++ script.js
@@ -9,3 +9,38 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+// Typing animation
+const typingText = document.querySelector(".typing-text");
+const textArray = [
+  "Python Developer 🐍",
+  "AI Enthusiast 🤖",
+  "Machine Learning Explorer 📊"
+];
+
+let textIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textIndex].length) {
+    typingText.textContent += textArray[textIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 1500);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingText.textContent = textArray[textIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    textIndex = (textIndex + 1) % textArray.length;
+    setTimeout(type, 1000);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  type();
+});
